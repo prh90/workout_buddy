ddf85df96a751aedc88479d2581b953fc74917ce
Pablo R. Hernandez <prh90@yahoo.com>
2017-03-17 19:51:59 -0700

    Adds simple CSS to root path. Added controllers with corresponding views, working on helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def current_user
+    if session[:user_id]
+      @user = User.find(session[:user_id])
+    end
+  end
+
+  def authorize
+    redirect_to login_path unless current_user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :home, only: :index
+  resources :users
+  resources :posts
+  
+  root 'home#index'
 end
