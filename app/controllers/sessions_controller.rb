e6a09c14db206ecceaad7616edbbf0b1da645c8a
Pablo R. Hernandez <prh90@yahoo.com>
2017-03-20 16:51:44 -0700

    Adds session grouping of controller, helper and session view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,22 @@
 class SessionsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by_email(params[:email])
+
+    if @user && @user.authenticate(params[:password])
+      log_in @user
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
+  end
+
 end
