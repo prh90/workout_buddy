e8643ebc3eb3d485bdf13059e3d2be6e0e2d5890
Pablo R. Hernandez <prh90@yahoo.com>
2017-03-22 16:44:46 -0700

    Adds style to center form, changes names in views for naming comvention

--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,12 @@
 ActiveRecord::Schema.define(version: 20170320235422) do
 
   create_table "users", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
