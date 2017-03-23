db7377d1a6fec508877f20f00e5483265d5074e5
Pablo R. Hernandez <prh90@yahoo.com>
2017-03-23 12:46:27 -0700

    Still debugging sign in MVC

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,28 @@
 class SessionsController < ApplicationController
+  def new
+    @user = User.new
+  end
 
   def create
-    @user = User.find_by_email(params[:email])
+    user = User.find_by(username: params[:username])
 
-    if @user && @user.authenticate(params[:password])
-      log_in @user
+    if user && user.authenticate(params[:password])
+      log_in(user)
+      flash[:notice] = "Hello #{user.username}!"
       redirect_to root_path
     else
-      render 'new'
+      flash[:error] = "Username or password was incorrect"
+      render :new
     end
   end
 
   def destroy
-    session[:user_id] = nil
-    redirect_to root_path
+    if user = User.find_by_id(session[:user_id])
+      log_out
+      redirect_to root_path
+    else
+      flash[:error] = "Failed to log out"
+      redirect_to root_path
+    end
   end
 end
