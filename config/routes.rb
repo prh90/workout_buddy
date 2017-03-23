2c14342660502f3b3e8ab5132144b19ce9358255
Pablo R. Hernandez <prh90@yahoo.com>
2017-03-23 15:52:08 -0700

    Adds code to fix bug

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
+  get 'register', to: 'users#new'
+
   root 'home#index'
 
 end
