60c7b23293231738c1f2a39ee19aded54d3acc5b
Pablo R. Hernandez <prh90@yahoo.com>
2017-03-23 17:58:58 -0700

    Adds fixed code for login page, fixed bug which only was naming convention problem

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,12 @@
 class SessionsController < ApplicationController
   def new
-    
+
   end
 
   def create
     @user = User.find_by(email: params[:email])
     if @user && @user.authenticate(params[:password])
-      login
+      login(@user)
       redirect_to root_path
     else
       @errors = ["You may have mistyped your email.", "You may have mistyped your password"]
